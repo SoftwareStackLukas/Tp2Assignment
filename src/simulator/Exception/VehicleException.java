a19918c81059f1792c2f48a50199252a932066fc
SoftwareStackLukas <83038634+SoftwareStackLukas@users.noreply.github.com>
2022-02-09 19:46:47 +0100

    Add files via upload

---
+++ src/simulator/Exception/VehicleException.java
@@ -0,0 +1,9 @@
+package simulator.Exception;
+
+public class VehicleException extends ParentException {
+    
+	public VehicleException (String message)    
+    {       
+        super(message);    
+    }   
+}
