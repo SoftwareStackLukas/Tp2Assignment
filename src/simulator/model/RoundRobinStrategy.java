67831791c4cc1191914bcb97c9e9edece192004e
SoftwareStackLukas <Lukas-Sebastian.Hofmann@gmx.de>
2022-02-10 19:33:29 +0100

    RoundRobinStrategy implemented and ready to be tested

--- src/simulator/model/RoundRobinStrategy.java
+++ src/simulator/model/RoundRobinStrategy.java
@@ -5,17 +5,28 @@ import java.util.List;
 public class RoundRobinStrategy implements LightSwitchStrategy {
 	private int timeSlot;
 	
-	
 	public RoundRobinStrategy(int timeSlot) {
 		this.timeSlot = timeSlot;
 	}
 	
-	
 	@Override
 	public int chooseNextGreen(List<Road> roads, List<List<Vehicle>> qs, int currGreen, int lastSwitchingTime,
 			int currTime) {
-		// TODO Auto-generated method stub
+		int result;
+		//If n list == null
+		try {
+			if (!roads.isEmpty()) {
+				if (currGreen == -1) {
+					result = 0;
+				} else if (currTime - lastSwitchingTime < timeSlot) {
+					result = currGreen;
+				} else {
+					result = (currGreen + 1) % roads.size();
+				}
+			} else result = -1;
+		} catch (Exception e) {
+			result = -1; 
+		}
 		return 0;
 	}
-
 }
