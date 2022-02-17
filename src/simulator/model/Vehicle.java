9b806534bc54e7e52d877a8ee15d90cd634a8d31
SoftwareStackLukas <Lukas-Sebastian.Hofmann@gmx.de>
2022-02-17 13:15:53 +0100

    moveToNextRoad is implemented. The class Vehicle still has to be tested

--- src/simulator/model/Vehicle.java
+++ src/simulator/model/Vehicle.java
@@ -9,13 +9,16 @@ import org.json.JSONObject;
 public class Vehicle extends SimulatedObject {
 	private List<Junction> itinerary;
 	private int maxSpeed;
-	private int currentSpeed;
+	private int currSpeed;
 	private VehicleStatus status;
 	private Road road;
-	private int location = 0;
-	private int contaminationClass;
-	private int totalContamination = 0;
-	private int totalTraveledDistance = 0;
+	private int location;
+	private int contClass;
+	private int totalContamination;
+	private int totalTraveledDistance;
+	
+	//To identify the junction
+	private int itineraryIdx;
 	
 	Vehicle(String id, int maxSpeed, int contClass, List<Junction> itinerary) {
 		super(id);
@@ -25,9 +28,17 @@ public class Vehicle extends SimulatedObject {
 				itinerary.size() >= 2)) {
 			throw new IllegalArgumentException("IllegalArguments");
 		}
+		//Create an unmuteable List
 		this.itinerary = Collections.unmodifiableList(new ArrayList<>(itinerary));
+		
 		this.maxSpeed = maxSpeed;
-		this.contaminationClass = contClass;
+		this.contClass = contClass;
+		this.currSpeed = 0; 
+		this.totalContamination = 0;
+		this.status = VehicleStatus.PENDING;
+		this.location = 0;
+		this.totalTraveledDistance = 0;
+		this.itineraryIdx = 0;	
 	}
 	
 	void setSpeed(int s) {
@@ -35,14 +46,14 @@ public class Vehicle extends SimulatedObject {
 		if (0 > toSetSpeed) {
 			throw new IllegalArgumentException("The speed is out of the legal range.");
 		} 
-		this.currentSpeed = toSetSpeed;
+		this.currSpeed = toSetSpeed;
 	}
 
 	void setContClass(int c) {
 		if (Math.subtractExact(10, c) < 0) {
 			throw new IllegalArgumentException("The contamination class has to be between [0,10]");
 		}
-		this.contaminationClass = c;
+		this.contClass = c;
 	}
 	
 	
@@ -51,31 +62,64 @@ public class Vehicle extends SimulatedObject {
 	void advance(int time) {
 		if (this.status == VehicleStatus.TRAVELING) {
 			int previewsLocation =  this.location;
-			this.location = Math.addExact(this.location, this.currentSpeed);
+			this.location = Math.addExact(this.location, this.currSpeed);
 			//the length of the current road;
 			int c = Math.multiplyExact(time, previewsLocation);
-			this.contaminationClass += c;
+			this.contClass += c;
 			road.addContamination(c);
 		}
+		
+//	modify this or take it // see after running the tests.
+//		if(this.status == VehicleStatus.TRAVELING) {
+//			//Update location
+//			int newLocation = Math.min(this.getLocation()+this.getCurrSpeed(), this.getRoad().getLength());
+//			int distTravelledOnCycle = newLocation - getLocation();		// For contamination calculus
+//			this.setLocation(newLocation);
+//			this.totalDistance += distTravelledOnCycle;
+//			
+//			//Update contamination
+//			int c = getContClass() * distTravelledOnCycle;
+//			setTotalContamination(getTotalContamination() + c);
+//			this.road.addContamination(c);			
+//			
+//			//Check end of road
+//			if(this.getLocation() == this.getRoad().getLength()) {
+//				this.setStatus(VehicleStatus.WAITING);
+//				this.setSpeed(0);
+//				itinerary.get(itineraryIdx).enter(this);
+//			}
+//		}
 	}
 
 	void moveToNextRoad() {
-		if (this.road != null) {
-			road.exit(this);
+		if (this.status == VehicleStatus.PENDING) {
+			// Starts the trip -> no previous road
+			this.road = itinerary.get(0).roadTo(itinerary.get(1));
+			this.road.enter(this);
+			itineraryIdx++;
+			this.status = VehicleStatus.TRAVELING;
 		}
-		
-		//After this the vehicle has a road
-		if (VehicleStatus.PENDING == this.status) {
-			this.road = this.itinerary.get(0).roadTo(this.itinerary.get(1));
-		} else {
-			for (Junction j : this.itinerary) {
-				if (true) {
-					this.road = this.itinerary.get(0).roadTo(this.itinerary.get(1));
-				}
+		else if (this.status == VehicleStatus.WAITING){
+			// Continues the trip
+			this.road.exit(this);			
+			if (itineraryIdx < itinerary.size() - 1) {
+				this.road = itinerary.get(itineraryIdx).roadTo(itinerary.get(itineraryIdx + 1));
+				this.location = 0;
+				this.currSpeed = 0;
+				this.status = VehicleStatus.TRAVELING;
+				this.road.enter(this);
+				itineraryIdx++;
+			}
+			else {
+				// Has arrived to end of itinerary
+				this.status = VehicleStatus.ARRIVED;
 			}
 		}
-		
-		road.enter(this);
+		else {
+			// Status = ARRIVED -> End the trip -> no new road -> throw exception
+			// Status = TRAVELLING -> Still moving on previous road -> throw exception			
+			throw new UnsupportedOperationException();
+		}
 	}
 	
 	@Override
@@ -83,10 +127,10 @@ public class Vehicle extends SimulatedObject {
 		JSONObject jo = new JSONObject();
 		
 		jo.put("id", this._id);
-		jo.put("speed", this.currentSpeed);
+		jo.put("speed", this.currSpeed);
 		jo.put("distance", this.totalTraveledDistance);
 		jo.put("co2", this.totalContamination);
-		jo.put("class", this.contaminationClass);
+		jo.put("class", this.contClass);
 		jo.put("status", this.status);
 		if (!(this.status == VehicleStatus.ARRIVED 
 				|| this.status == VehicleStatus.PENDING)) {
@@ -100,7 +144,7 @@ public class Vehicle extends SimulatedObject {
 	
 	//Class getters
 	int getSpeed() {
-		return this.currentSpeed;
+		return this.currSpeed;
 	}
 	
 	int getLocation() {
