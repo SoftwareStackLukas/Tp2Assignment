3e0223c660f94c6bd10759e2f84901bd291fa8ad
SoftwareStackLukas <Lukas-Sebastian.Hofmann@gmx.de>
2022-02-17 13:30:34 +0100

    Junction further

--- src/simulator/model/Junction.java
+++ src/simulator/model/Junction.java
@@ -1,5 +1,8 @@
 package simulator.model;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
@@ -13,8 +16,8 @@ public class Junction extends SimulatedObject {
 	private int lastSwitchingTime;
 	private LightSwitchStrategy lightSwitchingStrategy = null;
 	private DequeingStrategy dequeuingStrategy = null;
-	private int x;
-	private int y;
+	private int xCoor;
+	private int yCoor;
 	
 	Junction(String id, LightSwitchStrategy IsStrategy, DequeingStrategy
 			dqStrategy, int xCoor, int yCoor) {
@@ -28,13 +31,27 @@ public class Junction extends SimulatedObject {
 		if (xCoor < 0 || yCoor < 0) {
 			throw new IllegalArgumentException("Coordinat can not be negativ"); 
 		} else {
-			this.x = xCoor;
-			this.y = yCoor;
+			this.lightSwitchingStrategy = IsStrategy;
+			this.dequeuingStrategy = dqStrategy;
+			this.incomingRoads = new ArrayList<>();
+			this.outgoingRoads = new HashMap<>();
+			this.queues = new ArrayList<>();
+			
+			this.greenLightIndex = -1;
+			this.xCoor = xCoor;
+			this.yCoor = yCoor;
 		}
 	}
 	
 	void addIncommingRoad(Road r) {
-		
+		if (r.getDestJunc().equals(this)) {
+			this.incomingRoads.add(r);
+			LinkedList<Vehicle> waitingCars = new LinkedList<>();
+			this.queues.add(waitingCars);
+			this.			
+		} else {
+			
+		}
 	}
 	
 	void addOutGoingRoad(Road r) {
