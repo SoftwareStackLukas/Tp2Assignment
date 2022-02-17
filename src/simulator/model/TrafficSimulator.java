ee10cdcb684f4d098540f68e9e4cbee8f422d226
SoftwareStackLukas <Lukas-Sebastian.Hofmann@gmx.de>
2022-02-17 20:06:34 +0100

    TraficSimulator dummy

--- src/simulator/model/TrafficSimulator.java
+++ src/simulator/model/TrafficSimulator.java
@@ -1,5 +1,43 @@
 package simulator.model;
 
-public class TrafficSimulator {
+import java.util.List;
 
+import org.json.JSONObject;
+
+import simulator.misc.SortedArrayList;
+
+public class TrafficSimulator {
+	private RoadMap roadMap;
+	private List<Event> events;
+	private int simulationTime;
+	
+	public TrafficSimulator() {
+		this.init();
+	}
+	
+	private void init() {
+		this.roadMap = new RoadMap();
+		this.simulationTime = 0;
+		this.events = new SortedArrayList<>();
+	}
+	
+	public void addEvent(Event e) {
+		//TODO ...
+	}
+	
+	public void advance() {
+		//TODO ...
+	}
+	
+	public void reset() {
+		//TODO ...
+	}
+	
+	public JSONObject report() {
+		JSONObject jo = new JSONObject();
+		//TODO ...
+		return jo;
+	}
+	
+	
 }
