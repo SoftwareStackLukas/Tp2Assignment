f97f1c1e58fe84969a518f30b0786900b67f1dca
SoftwareStackLukas <Lukas-Sebastian.Hofmann@gmx.de>
2022-02-23 14:24:54 +0100

    NewVehicleEvent first implemented

---
+++ src/simulator/model/NewSetContClassEvent.java
@@ -0,0 +1,16 @@
+package simulator.model;
+
+public class NewSetContClassEvent extends Event {
+
+	public NewSetContClassEvent(int time, List<Pair<String,Integer> cs) {
+		super(time);
+		// ...
+	}
+	
+	@Override
+	void execute(RoadMap map) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- src/simulator/model/NewVehicleEvent.java
+++ src/simulator/model/NewVehicleEvent.java
@@ -1,18 +1,27 @@
 package simulator.model;
 
-public class NewVehicleEvent extends Event {
+import java.util.List;
 
+public class NewVehicleEvent extends Event {
+	private String id;
+	private int maxSpeed;
+	private int contClass;
+	private List <String> itinerary;
+	
+	
 	public NewVehicleEvent(int time, String id, int maxSpeed, int
 			contClass, List<String> itinerary) {
 			super(time);
-			// ...
+			this.id = id;
+			this.maxSpeed = maxSpeed;
+			this.contClass = contClass;
+			this.itinerary = itinerary;
 	}
 	
 	@Override
 	void execute(RoadMap map) {
-		// TODO Auto-generated method stub
-
+		Vehicle v = new Vehicle(id, maxSpeed, contClass, itinerary);
+		map.addVehicle(v);
+		v.moveToNextRoad();
 	}
-
-
 }
