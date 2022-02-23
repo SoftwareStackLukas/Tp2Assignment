93d83c0daa813bc98431a8d3a72b1d6a587e81e5
SoftwareStackLukas <Lukas-Sebastian.Hofmann@gmx.de>
2022-02-23 15:24:32 +0100

    NeSetContClassEvent

--- src/simulator/model/NewSetContClassEvent.java
+++ src/simulator/model/NewSetContClassEvent.java
@@ -1,16 +1,27 @@
 package simulator.model;
 
-public class NewSetContClassEvent extends Event {
+import java.util.List;
 
-	public NewSetContClassEvent(int time, List<Pair<String,Integer> cs) {
+import simulator.misc.Pair;
+
+public class NewSetContClassEvent extends Event {
+	private List<Pair<String,Integer>> cs;
+	
+	public NewSetContClassEvent(int time, List<Pair<String,Integer>> cs) {
 		super(time);
-		// ...
+		if (cs == null) {
+			throw new NullPointerException("cs can not be null");
+		} else {
+			this.cs = cs;
+		}
 	}
 	
 	@Override
 	void execute(RoadMap map) {
-		// TODO Auto-generated method stub
-
+		for (Pair<String, Integer> c : cs) {
+			Vehicle tempV = map.getVehicle(c.getFirst());
+			if (tempV == null) throw new NullPointerException("Vehicle is null");
+			tempV.setContClass(c.getSecond());
+		}
 	}
-
 }
