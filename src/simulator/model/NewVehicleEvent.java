0f24bfb81975777d5d8800c1b310045251b47305
SoftwareStackLukas <Lukas-Sebastian.Hofmann@gmx.de>
2022-02-23 13:58:48 +0100

    NewVeEv

--- src/simulator/model/NewVehicleEvent.java
+++ src/simulator/model/NewVehicleEvent.java
@@ -2,10 +2,17 @@ package simulator.model;
 
 public class NewVehicleEvent extends Event {
 
+	public NewVehicleEvent(int time, String id, int maxSpeed, int
+			contClass, List<String> itinerary) {
+			super(time);
+			// ...
+	}
+	
 	@Override
 	void execute(RoadMap map) {
 		// TODO Auto-generated method stub
 
 	}
+
 
 }
