25fe65256fd445c2fab65837788a2394e5f4ca5c
SoftwareStackLukas <Lukas-Sebastian.Hofmann@gmx.de>
2022-05-06 13:15:27 +0200

    The interface is fine and the main logic also. However the table is not showing the data

--- src/simulator/view/SpeedDialog.java
+++ src/simulator/view/SpeedDialog.java
@@ -1,16 +1,29 @@
 package simulator.view;
 
+import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.LayoutManager;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
+import javax.swing.BorderFactory;
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
+import javax.swing.JComponent;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.JSpinner;
+import javax.swing.JTable;
+import javax.swing.SpinnerModel;
+import javax.swing.SpinnerNumberModel;
+import javax.swing.border.TitledBorder;
 import javax.swing.table.AbstractTableModel;
 
 import simulator.control.Controller;
@@ -21,12 +34,20 @@ import simulator.model.Vehicle;
 
 public class SpeedDialog extends MyDialog {
 	private final static String HELP_TEXT = "Select a speed limit and press UPDATE to show the vehicels that exceed this speed in each tick.";
+	private final static String TICKER_LABEL = "Speed Limit";
+	private final static String TICKER_HELP_TEXT = "Increase or degrease the Limit";
+	private final static int TICK_DEFAULT = 60;
+	private final static int TICK_MIN = 0;
+	private final static int TICK_MAX = Integer.MAX_VALUE;
+	private final static int TICK_STEP = 1;
+	
 	private int sLimit;
 	private JLabel speedLimit;
 	private JSpinner speedSpinner;
 	private JButton close;
 	private JButton update;
 	private innerTable table;
+	private GridBagConstraints constraints;
 	
 	//Vehicle logic
 	private int tickCounter = 1;
@@ -43,23 +64,71 @@ public class SpeedDialog extends MyDialog {
 		setTitle("Vehicles Speed History");
 		mainPanel.setLayout( new BoxLayout( mainPanel, BoxLayout.Y_AXIS) );
 		setContentPane(mainPanel);
-		
 		initHelpPanel(SpeedDialog.HELP_TEXT);
 		
+		JPanel spinnerPanel = new JPanel();
+		spinnerPanel.setAlignmentY(JComponent.TOP_ALIGNMENT);
+		this.mainPanel.add(spinnerPanel);
+		//Add label + spinner
+		this.speedLimit = new JLabel(SpeedDialog.TICKER_LABEL);	
+		SpinnerModel model = new SpinnerNumberModel(SpeedDialog.TICK_DEFAULT, //initial value
+				   								    SpeedDialog.TICK_MIN, //min
+				   								  	SpeedDialog.TICK_MAX, //max
+				   								  	SpeedDialog.TICK_STEP); //step
+		this.speedSpinner = this.initSpinner(model);
+		spinnerPanel.add(this.speedLimit);
+		spinnerPanel.add(this.speedSpinner);
+		
+		JPanel buttonPanel = new JPanel();
+		buttonPanel.setAlignmentY(JComponent.CENTER_ALIGNMENT);
+		this.mainPanel.add(buttonPanel);
+		//JPanel buttonPanel = new JPanel(new BorderLayout());
 		this.close = new JButton("Close");
 		this.close.addActionListener( (e) -> {
 			closeOption = CANCEL;
+			this.deregisterObservers();
 			dispose();
 		});
+		this.close.setSize(new Dimension (50,30));
+		buttonPanel.add(this.close);
+		
 		//Button has to be added 
 		this.update = new JButton("Update");
+		this.update.setLocation(30, 50);
 		this.update.addActionListener((e) -> {
-			table.fireTableStructureChanged();
+			table.fireTableDataChanged();
 		});
-		//Button has to be added		
+		this.update.setSize(new Dimension (50,30));
+		buttonPanel.add(update);
 		
-		setMinimumSize(new Dimension(100, 100));
-		setPreferredSize(new Dimension(500, 250));
+		JPanel viewsPanel = new JPanel(new GridBagLayout());
+		//Build gridconstraints
+		constraints = new GridBagConstraints();
+		constraints.fill = GridBagConstraints.BOTH;
+		constraints.weightx = 0.5;
+		constraints.weighty = 0.5;
+		constraints.gridx = 0;
+		constraints.gridy = 0;
+		//End Build
+		
+		JTable t = new JTable(new innerTable());
+		t.setFillsViewportHeight(true);
+	    JPanel eventsView = createViewPanel(t, "Events");
+	    viewsPanel.add(eventsView, constraints);
+	    this.mainPanel.add(viewsPanel);
+	    
+		setMinimumSize(new Dimension(100, 500));
+		setPreferredSize(new Dimension(500, 500));
+	}
+	
+	private JSpinner initSpinner(SpinnerModel model) {
+		JSpinner speedSpinner = new JSpinner(model);
+		speedSpinner.setToolTipText(TICKER_LABEL);
+		speedSpinner.setToolTipText(SpeedDialog.TICKER_HELP_TEXT);
+		speedSpinner.setMaximumSize(new Dimension(80, 40));
+		speedSpinner.setMinimumSize(new Dimension(80, 40));
+		speedSpinner.setPreferredSize(new Dimension(80, 40));
+		return speedSpinner;
 	}
 	
 	@Override
@@ -84,7 +153,7 @@ public class SpeedDialog extends MyDialog {
 		
 		@Override
 		public int getRowCount() {
-			return SpeedDialog.this.ticks.size();
+			return SpeedDialog.this.ticks != null ? SpeedDialog.this.ticks.size() : 0;
 		}
 
 		@Override
@@ -102,7 +171,7 @@ public class SpeedDialog extends MyDialog {
 				sb.append("[");
 				List<Pair<Integer, String>> ls = SpeedDialog.this.ticks.get(rowIndex);
 				for (Pair<Integer, String> p : ls) {
-					if (SpeedDialog.this.sLimit > p.getFirst()) sb.append(p.getSecond() + ",");
+					if (SpeedDialog.this.sLimit < p.getFirst()) sb.append(p.getSecond() + ",");
 				}
 				int length = sb.length();
 				sb.replace(length, length, "");
@@ -121,6 +190,20 @@ public class SpeedDialog extends MyDialog {
 		public boolean isCellEditable(int row, int col) {
 			return false;
 		}
+	}
+	
+	private JPanel createViewPanel(JComponent c, String title) {
+		JPanel p = new JPanel(new BorderLayout());
+		p.setBorder(BorderFactory.createTitledBorder(BorderFactory.createLineBorder(Color.black), title,
+													TitledBorder.LEFT, TitledBorder.TOP)); 
+		
+		JScrollPane sp = new JScrollPane(c);
+		sp.setBorder(BorderFactory.createEmptyBorder());
+		p.setBackground(MainWindow.mainColor);
+		p.add(sp);
+		p.getAutoscrolls();
+		
+		return p;
 	}
 
 }
